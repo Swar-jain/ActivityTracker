e1ce79f0bcc257669310214a0e93627f5992ee55
Swar-jain <jain.swarnalatha@gmail.com>
2019-11-23 09:21:53 -0700

    test

--- ActivityJS.js
+++ ActivityJS.js
@@ -1,12 +1,6 @@
-$(document).ready(function(){
-		    $('.count').prop('disabled', true);
-   			$(document).on('click','.plus',function(){
-				$('.count').val(parseInt($('.count').val()) + 1 );
-    		});
-        	$(document).on('click','.minus',function(){
-    			$('.count').val(parseInt($('.count').val()) - 1 );
-    				if ($('.count').val() == 0) {
-						$('.count').val(1);
-					}
-    	    	});
- 		});
+function myFunction() {
+ var i =  document.getElementById("hp").textContent;
+ var j = parseInt(i, 10);
+ j = j-1;
+ document.getElementById("hp").innerHTML = j;
+}
